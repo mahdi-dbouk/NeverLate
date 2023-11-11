48549d7bce531cea54f38f49540fced7a390d64b
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-11 12:22:34 +0200

    [Client] Installed and Configured TailwindCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div></div>
-  )
+    <div>
+      <h1 className="m-auto h-4 w-5 rounded-full bg-slate-50 p-2 text-red-600">
+        Hello World
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
