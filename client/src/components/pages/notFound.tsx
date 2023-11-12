b7b520281df5c7c0006840afb73a32eed8022c14
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-12 23:51:53 +0200

    [Client] Implemented UI for NotFound Page

---
+++ client/src/components/pages/notFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-full w-full flex-row items-center justify-center">
+      <span className="text-5xl font-bold text-black">
+        Woopss...Page Not Found!
+      </span>
+    </div>
+  );
+};
+
+export default NotFound;
