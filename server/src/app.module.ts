dfe2012b4ce666752a8f96b923763b239ea14b08
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-12 23:55:43 +0200

    [Server] Configured App Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TodosModule } from './todos/todos.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/users.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule, PrismaModule, UserModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
