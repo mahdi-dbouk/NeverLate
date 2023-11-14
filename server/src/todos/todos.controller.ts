c9e39396e412787de34db84d676d246006e45155
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-14 17:14:34 +0200

    [Server] Updated Todos Controller

--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,17 +1,59 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './DTO/createTodo.dto';
+import { UpdateTodoDto } from './DTO/updateTodo.dto';
+import { JWTAuthGuard } from 'src/auth/auth.guard';
+import { AuthRequest } from 'src/interfaces/auth-request.interface';
 
 @Controller('todos')
+@UseGuards(JWTAuthGuard)
 export class TodosController {
   constructor(private TodosService: TodosService) {}
 
   @Get()
-  getAllTodos(@Body('userId') userId: number): any {
-    return this.TodosService.getAllTodos(userId);
+  getAllTodos(@Req() request: AuthRequest): any {
+    const token = request.token;
+    return this.TodosService.getAllTodos(token);
   }
   @Post('create')
-  createTodo(@Body() createTodoDto: CreateTodoDto) {
-    return this.TodosService.createTodo(createTodoDto);
+  createTodo(
+    @Req() request: AuthRequest,
+    @Body() createTodoDto: CreateTodoDto,
+  ) {
+    const token = request.token;
+    return this.TodosService.createTodo(token, createTodoDto);
+  }
+
+  @Put('update/:id')
+  updateTodoById(
+    @Param('id') id: string,
+    @Req() request: AuthRequest,
+    @Body() updateTodoDto: UpdateTodoDto,
+  ) {
+    const token = request.token;
+    return this.TodosService.updateTodoById(parseInt(id), token, updateTodoDto);
+  }
+
+  @Patch('update/:id/status')
+  markCompleted(@Param('id') id: string, @Req() request: AuthRequest) {
+    const token = request.token;
+    return this.TodosService.markCompleted(parseInt(id), token);
+  }
+
+  @Delete('delete/:id')
+  deleteTodoById(@Req() request: AuthRequest, @Param('id') id: string) {
+    const token = request.token;
+    return this.TodosService.deleteTodoById(token, parseInt(id));
   }
 }
