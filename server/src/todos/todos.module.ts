a2abd1ab3e86f6c7f0ca44b8e06ad99b38a4771f
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-14 17:13:13 +0200

    [Server] Added JWT Service to Todos Module

--- server/src/todos/todos.module.ts
+++ server/src/todos/todos.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PrismaModule],
   controllers: [TodosController],
-  providers: [TodosService],
+  providers: [TodosService, JwtService],
 })
 export class TodosModule {}
