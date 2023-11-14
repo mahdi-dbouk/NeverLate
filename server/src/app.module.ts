a9c4d57bd99236ed82401cf2d78ef22384407709
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-14 17:17:36 +0200

    [Server] Added Auth Module to App Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { TodosModule } from './todos/todos.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, PrismaModule, UserModule],
+  imports: [TodosModule, PrismaModule, UserModule, AuthModule],
   providers: [PrismaService],
 })
 export class AppModule {}
