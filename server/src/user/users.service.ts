08afbccddedc5bb1fd251ca96dad64a7e69111e6
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-14 17:15:47 +0200

    [Server] Removed Login Functionality From User Service

--- server/src/user/users.service.ts
+++ server/src/user/users.service.ts
@@ -1,9 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './DTO/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { User } from '@prisma/client';
-import { UserCredentialsDto } from './DTO/user-credentials.dto';
 
 @Injectable()
 export class UserService {
@@ -11,6 +10,16 @@ export class UserService {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password } = createUserDto;
+    const isUserFound = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isUserFound) {
+      throw new ConflictException('User already exists');
+    }
+
     const newUser = await this.prisma.user.create({
       data: {
         name,
@@ -19,34 +28,5 @@ export class UserService {
       },
     });
     return newUser;
-  }
-
-  async login(userCredentialsDto: UserCredentialsDto) {
-    const { email, password } = userCredentialsDto;
-    const authUser = this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        hashedPassword: true,
-      },
-    });
-    console.log(await authUser);
-    if (await authUser) {
-      const isPasswordCorrect = await bcrypt.compare(
-        password,
-        (await authUser).hashedPassword,
-      );
-      if (isPasswordCorrect) {
-        return { email: (await authUser).email, name: (await authUser).name, id: (await authUser).id };
-      } else {
-        return { error: 'user not found' };
-      }
-    } else {
-      return { error: 'user not found' };
-    }
   }
 }
