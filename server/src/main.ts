73151d5fe13b6e5e4939e00e8295e428ea004df5
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-14 17:08:53 +0200

    [Server] Implemented CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = {
+    origin: ['*'],
+    methods: 'GET, POST, PUT, HEAD, DELETE, UPDATE, PATCH, OPTIONS',
+  };
+  app.enableCors(cors);
   await app.listen(3000);
 }
 bootstrap();
