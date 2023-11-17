fd5052ab635e4465d7deb21426af67d79e7cbb72
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-17 15:55:13 +0200

    [Client] Added Routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client"
 import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AuthPage from "./components/pages/authPage.tsx";
-import NotFound from "./components/pages/notFound.tsx";
 
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/auth",
-    element: <AuthPage />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
