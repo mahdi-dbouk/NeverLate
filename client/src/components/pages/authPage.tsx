43529691cbc33c94df379191b113988c1cc2fab0
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-17 15:54:15 +0200

    [Client] Updated AuthForm Element

--- client/src/components/pages/authPage.tsx
+++ client/src/components/pages/authPage.tsx
@@ -3,7 +3,7 @@ import AuthForm from "../ui/authForm";
 const AuthPage = () => {
     return (
     <div className="flex h-full w-full flex-col gap-2 items-center justify-center">
-        <AuthForm title="Login" submit={() => {}}/>
+        <AuthForm />
       </div>
     )
 }
