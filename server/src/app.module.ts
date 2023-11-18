eadd7928cb0b526419e547834e66b01e2a4a29a6
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-18 12:39:08 +0200

    [Server] Enabled .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TodosModule } from './todos/todos.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/users.module';
 import { AuthModule } from './auth/auth.module';
-
 @Module({
-  imports: [TodosModule, PrismaModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TodosModule,
+    PrismaModule,
+    UserModule,
+    AuthModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
