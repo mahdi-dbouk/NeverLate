47f97766111e5ca7e36ca5ff4245ccf4f3810196
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-21 18:13:10 +0200

    [Server] Updated CORS Configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const cors = {
-    origin: ['*'],
+    origin: 'http://localhost:5173',
     methods: 'GET, POST, PUT, HEAD, DELETE, UPDATE, PATCH, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   };
   app.enableCors(cors);
   await app.listen(3000);
