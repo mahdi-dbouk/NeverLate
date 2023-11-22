a66835efea470d394a7db99c5ce17a3388ba85e8
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 15:23:31 +0200

    [Client] Refactored TodoList Component

--- client/src/components/pages/todosList.tsx
+++ client/src/components/pages/todosList.tsx
@@ -1,27 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import Todo from "../ui/todo";
-import axios from "axios";
 import Snackbar from "../shared/snackbar";
 import { useEffect, useRef, useState } from "react";
 import TodoLoading from "../ui/todoLoading";
-import { groupTodosByDate } from "../../utilities/todo.utilities";
+import { groupTodosByDate, groupTodosByStatus, sortTodosByDateAndPriority } from "../../utilities/todo.utilities";
+import { getAllTodos } from "../../services/todos.service";
+import { TodoType } from "../../types/todo.types";
+import { useNavigate } from "react-router-dom";
+import { deleteAuthUser } from "../../utilities/localstorage.utilities";
 
 
-const TodoList = () => {
+const TodoList: React.FC<{showCompleted: boolean}> = ({showCompleted}) => {
   const token = localStorage.getItem('access-token')
-  console.log(token)
   const [snackbarMessage, setSnackbarMessage] = useState<string>("");
   const [snackbarStatus, setSnackbarStatus] = useState<string>("");
+  const [pendingTodos, setPendingTodos] = useState<TodoType[]>([])
+  const [completedTodos, setCompletedTodos] = useState<TodoType[]>([])
+  const navigate = useNavigate();
+  
   const snackbarRef = useRef<React.ElementRef<typeof Snackbar>>(null);
 
-  const {data, isLoading, isError, isSuccess} = useQuery({
+  const {data, isLoading, isError, error, isSuccess} = useQuery({
     queryKey: ['todos'],
     queryFn: async () => {
-      const response = await axios.get('http://localhost:3000/todos', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
+      const response = await getAllTodos();
       return response.data
     },
     staleTime: 20 * 1000,
@@ -29,7 +31,19 @@ const TodoList = () => {
   })
 
   useEffect(() => {
+    if(isSuccess){
+      const groupedByStatusTodos = isSuccess? groupTodosByStatus(data): null;
+      setPendingTodos(groupedByStatusTodos!.pending);
+      setCompletedTodos(groupedByStatusTodos!.completed);
+    }
+  }, [data, isSuccess])
+
+  useEffect(() => {
     if (isError) {
+      if(error.response.status === 401) {
+        deleteAuthUser()
+        navigate('/', {replace: true})
+      }
       setSnackbarMessage('Oops..Something went wrong!');
       setSnackbarStatus('failed');
       snackbarRef.current?.show();
@@ -37,14 +51,22 @@ const TodoList = () => {
         snackbarRef.current?.hide();
       }, 3600);
     }
-  }, [isError]);
+  }, [navigate, error, isError]);
 
-  const groupedByDateTodos = isSuccess? groupTodosByDate(data): [];
+  let groupedByDateTodos;
+  if(showCompleted){
+    groupedByDateTodos = isSuccess? groupTodosByDate(completedTodos??[]):groupTodosByDate([]);
+  } else {
+    groupedByDateTodos = isSuccess? groupTodosByDate(pendingTodos??[]):groupTodosByDate([]);
+  }
+
+  const sortedByDateAndPriorityTodos = sortTodosByDateAndPriority(groupedByDateTodos);
+
   return (
     <div className="flex flex-col items-center justify-center gap-2 pb-4 pt-20">
       <Snackbar ref={snackbarRef} message={snackbarMessage} status={snackbarStatus}/>
       {
-        isSuccess && Object.entries(groupedByDateTodos).map(([date, todosForDate]) => (
+        isSuccess && Object.entries(sortedByDateAndPriorityTodos).map(([date, todosForDate]) => (
           <>
             <div  key={date} className="flex h-12 w-2/5 flex-row items-center justify-start">
               <span className="text-md font-medium text-gray-500">
@@ -58,9 +80,9 @@ const TodoList = () => {
                   : new Date(date).toDateString()}
               </span>
             </div>
-            {todosForDate.map((todo, index) => (
+            {todosForDate.map((todo) => (
               <Todo
-                key={index}
+                key={todo.id}
                 id={todo.id}
                 description={todo.description}
                 date={todo.date}
