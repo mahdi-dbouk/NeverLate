1f60a291978318d63c02c3630acc350cbd009b0a
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 11:18:37 +0200

    [Server] Updated Local Storage Utility

--- client/src/utilities/localstorage.utilities.ts
+++ client/src/utilities/localstorage.utilities.ts
@@ -1,8 +1,15 @@
 import { LoginResponse } from "../types/auth.types";
 
 export const saveAuthUser = (data: LoginResponse) => {
+    localStorage.setItem('access-token', data.token);
     localStorage.setItem('auth-user-email', data.data.email);
     localStorage.setItem('auth-user-name', data.data.name);
-    localStorage.setItem('auth-user-id', data.data.id.toString())
-    localStorage.setItem('access-token', data.token)
+    localStorage.setItem('auth-user-id', data.data.id.toString());
+}
+
+export const deleteAuthUser = () => {
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('auth-user-email');
+    localStorage.removeItem('auth-user-name');
+    localStorage.removeItem('auth-user-id');
 }
