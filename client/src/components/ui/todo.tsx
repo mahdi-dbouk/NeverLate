7dd80164ff35e4ce1303524d4208c92f28a26353
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 12:12:17 +0200

    [Client] Implemented Logic For Update and Delete On Todo Items

--- client/src/components/ui/todo.tsx
+++ client/src/components/ui/todo.tsx
@@ -5,16 +5,103 @@ import { faEdit } from "@fortawesome/free-solid-svg-icons/faEdit";
 import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
 import classNames from "classnames";
 import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+import { useRef, useState } from "react";
+import CreateTodoModal from "./createTodoModal";
+import Snackbar from "../shared/snackbar";
+import { CreateTodoResponse } from "../../types/todo.types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteTodoById, markTodoCompleted } from "../../services/todos.service";
 
 type TodoProps = {
+  id: number
   description?: string;
   priority?: string;
   date?: string;
-  status?: boolean;
+  status?: string;
 };
 
-const Todo: React.FC<TodoProps> = ({ description, priority, date, status }) => {
+const Todo: React.FC<TodoProps> = ({id, description, priority, date, status }) => {
+
+  const createTodoModalRef = useRef<React.ElementRef<typeof CreateTodoModal>>(null)
+  const queryClient = useQueryClient();
+  const snackbarRef = useRef<React.ElementRef<typeof Snackbar>>(null);
+
+  const [snackbarMessage, setSnackbarMessage] = useState<string>("");
+  const [snackbarStatus, setSnackbarStatus] = useState<string>("");
+
+  const markCompletedMutation = useMutation({
+    mutationKey: ['todos'],
+    mutationFn: async (id: number) => {
+      const response = await markTodoCompleted(id);
+
+      return response.data;
+    },
+    onSuccess: (data: CreateTodoResponse) => {
+      console.log(data)
+      queryClient.invalidateQueries({queryKey: ['todos']});
+      snackbarRef.current?.show();
+      setSnackbarMessage("Status Updated Successfully!");
+      setSnackbarStatus("success");
+      setTimeout(() => {
+        snackbarRef.current?.hide();
+      }, 3600);
+    },
+    onError: () => {
+      queryClient.invalidateQueries({queryKey: ['todos']});
+      snackbarRef.current?.show();
+      setSnackbarMessage("Oops...Something went wrong!");
+      setSnackbarStatus("failed");
+      setTimeout(() => {
+        snackbarRef.current?.hide();
+      }, 3600);
+    }, 
+
+  });
+
+  const deleteTodoByIdMutation = useMutation({
+    mutationKey: ['todos'],
+    mutationFn: async (id: number) => {
+      const response = await deleteTodoById(id);
+
+      return response.data;
+    },
+    onSuccess: (data: CreateTodoResponse) => {
+      console.log(data)
+      queryClient.invalidateQueries({queryKey: ['todos']});
+      snackbarRef.current?.show();
+      setSnackbarMessage("Todo Deleted Successfully!");
+      setSnackbarStatus("success");
+      setTimeout(() => {
+        snackbarRef.current?.hide();
+      }, 3600);
+    },
+    onError: () => {
+      queryClient.invalidateQueries({queryKey: ['todos']});
+      snackbarRef.current?.show();
+      setSnackbarMessage("Oops...Something went wrong!");
+      setSnackbarStatus("failed");
+      setTimeout(() => {
+        snackbarRef.current?.hide();
+      }, 3600);
+    }, 
+
+  })
+
+
+
+  const markCompleted = () => {
+    markCompletedMutation.mutate(id);
+  }
+
+  const deleteTodo = () => {
+    deleteTodoByIdMutation.mutate(id);
+  }
+
+
   return (
+    <>
+    <CreateTodoModal todoId={id} ref={createTodoModalRef} />
+    <Snackbar ref={snackbarRef} message={snackbarMessage} status={snackbarStatus} /> 
     <div className="h-36 w-2/5 border bg-white px-2 shadow-md">
       <div className="flex flex-col">
         <div className="flex h-8 w-full flex-row items-center justify-between pt-2">
@@ -22,13 +109,13 @@ const Todo: React.FC<TodoProps> = ({ description, priority, date, status }) => {
             className={classNames(
               "flex h-[24px] w-fit flex-row items-center justify-center rounded-full px-2 py-1",
               {
-                "bg-green-600": status,
-                "bg-amber-400": !status,
+                "bg-green-600": status === 'completed',
+                "bg-amber-400": status === 'pending',
               },
             )}
           >
             <span className="text-xs font-bold text-white">
-              {status ? "Completed" : "Pending"}
+              {status === 'completed' ? "Completed" : "Pending"}
             </span>
           </div>
           <div className="flex h-[24px] w-fit flex-row items-center justify-center gap-1 px-2 py-1">
@@ -68,7 +155,7 @@ const Todo: React.FC<TodoProps> = ({ description, priority, date, status }) => {
               icon={faCheck}
               iconColor="white"
               disabled={false}
-              action={() => {}}
+              action={() => {markCompleted()}}
             />
             <Button
               variant="icon"
@@ -77,7 +164,7 @@ const Todo: React.FC<TodoProps> = ({ description, priority, date, status }) => {
               icon={faEdit}
               iconColor="white"
               disabled={false}
-              action={() => {}}
+              action={() => {createTodoModalRef.current?.openModal('update')}}
             />
             <Button
               variant="ghost-icon"
@@ -89,12 +176,13 @@ const Todo: React.FC<TodoProps> = ({ description, priority, date, status }) => {
               buttonColor="border-rose-500"
               onHoverColor="hover:border-rose-600"
               disabled={false}
-              action={() => {}}
+              action={() => {deleteTodo()}}
             />
           </div>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
