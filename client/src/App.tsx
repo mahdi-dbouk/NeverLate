073ac9a4cce8a442d261ea4af565c76e70a0388a
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 12:16:17 +0200

    [Client] Added NotFound Route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./components/pages/authPage";
 import Dashboard from "./components/pages/dashboard";
+import NotFound from "./components/pages/notFound";
 
 function App() {
   return (
     <Routes>
       <Route element={<AuthPage />} path="/auth"/>
       <Route element={<Dashboard />} path="/dashboard"/>
+      <Route element={<NotFound />} path="*" />
     </Routes>
   );
 }
