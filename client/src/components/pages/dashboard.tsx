55dd958e3f0d2bc351a20fd22266ad58e7ba6b45
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 15:24:13 +0200

    [Client] Implemented Logic To View Completed Todos

--- client/src/components/pages/dashboard.tsx
+++ client/src/components/pages/dashboard.tsx
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import Navbar from '../ui/Navbar';
 import FloatingActions from '../ui/floatingActions';
 import TodoList from './todosList';
 
 const Dashboard: React.FC = () => {
+    const [showCompleted, setShowCompleted] = useState<boolean>(false)
     return(
         <>
             <Navbar email={localStorage.getItem('auth-user-email') ?? "username"} />
-            <TodoList />
-            <FloatingActions />
+            <TodoList showCompleted={showCompleted} />
+            <FloatingActions showCompleted={showCompleted} setShowCompleted={setShowCompleted}/>
         </>
     )
 }
