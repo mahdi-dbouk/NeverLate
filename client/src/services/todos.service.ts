644c14dcd911a0397ce023f5db7ff48e258d89bd
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 11:19:18 +0200

    [Client] Updated Token Retrieval in Todos Service

---
+++ client/src/services/todos.service.ts
@@ -0,0 +1,69 @@
+import axios, { AxiosResponse } from "axios";
+import { CreateTodoRequest, CreateTodoResponse, UpdateTodoRequest } from "../types/todo.types";
+
+
+const getToken = (): string | null => {
+    return localStorage.getItem('access-token');
+}
+
+
+export const createTodo = async (
+    data: CreateTodoRequest,
+  ): Promise<AxiosResponse<CreateTodoResponse>> => {
+    try {
+        const token = getToken();
+      return await axios.post("http://localhost:3000/todos/create", data, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+export const updateTodoById = async (
+    data: UpdateTodoRequest,
+  ): Promise<AxiosResponse<CreateTodoResponse>> => {
+    try {
+        const token = getToken();
+      return await axios.put(`http://localhost:3000/todos/update/${data.id}`, data, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  export const markTodoCompleted = async (id: number): Promise<AxiosResponse<CreateTodoResponse>> => {
+    try {
+        const token = getToken();
+        return await axios.patch(`http://localhost:3000/todos/update/${id}/status`,{}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+  }
+
+  export const deleteTodoById = async (id: number): Promise<AxiosResponse<CreateTodoResponse>> => {
+    try {
+        const token = getToken();
+        return await axios.delete(`http://localhost:3000/todos/delete/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+  }
+
