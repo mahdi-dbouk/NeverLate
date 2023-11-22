1131360b91767613bf9efdceda139c5af11a3ab0
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-22 15:24:40 +0200

    [Client] Commented React-Query DevTools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import "./index.css";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+//<- Devtools ->
+//import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactModal from "react-modal";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,12 +17,14 @@ const queryClient = new QueryClient({
   }
 });
 
+ReactModal.setAppElement('#root');
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
+{/*         <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} /> */}
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
